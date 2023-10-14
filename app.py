629e0de54d6d6fad7e827e169e6a83dfa935dfff
Alexsandr Mazurov <mazurovsasha@gmail.com>
2023-10-14 19:51:58 +0000

    Start v6 new app

--- app.py
+++ app.py
@@ -1,7 +1,26 @@
-from flask import Flask, render_template, request
+# from flask import Flask, render_template, request
 
+
+# app = Flask(__name__)
+
+# @app.route('/')
+# def index():
+#     return render_template('index.html')
+
+# @app.route('/hello', methods=['POST'])
+# def hello():
+#     name = request.form.get('name')
+#     return render_template('hello.html', name=name)
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
+
+from flask import Flask, render_template, request
+from flask_frozen import Freezer
 
 app = Flask(__name__)
+freezer = Freezer(app)
 
 @app.route('/')
 def index():
@@ -13,4 +32,5 @@ def hello():
     return render_template('hello.html', name=name)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.debug = True
+    freezer.freeze()
